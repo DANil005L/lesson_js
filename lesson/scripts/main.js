90534a8f55d0d17375e2de1e447071c58c52a3d2
DANiL005L <danikubar@gmail.com>
2024-04-30 18:13:19 +0300

    finish commit

---
+++ lesson/scripts/main.js
@@ -0,0 +1,29 @@
+var a="hello"
+console.log(a)
+var b="owu"
+console.log(b)
+var c= "ua"
+console.log(c)
+let d =1;
+console.log(d)
+let e =10;
+console.log(e)
+let f =-999;
+console.log(f)
+let g =123;
+console.log(g)
+let i =3.14;
+console.log(i)
+let k =2.7;
+console.log(k)
+let w =16;
+console.log(w)
+const r= true
+console.log(r)
+const y =false
+console.log(y)
+
+
+
+
+
